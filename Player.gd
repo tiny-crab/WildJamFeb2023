364a7cb370f7c1df4deaa54a59f3948b5548fa85
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2023-02-12 14:07:09 -0800

    Fix unlimited jumps
    Player can no longer jump infinitely, jump will not reset until player
    touches the ground again. removed horizontal friction while jumping, for
    better feeling jumps. Also added a new boolean variable called
    STATE_DEBUG used for printing out debug messages about player state.

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+
+const UP = Vector2(0, -1)
 const GRAVITY = 600.0
 const ACCELERATION = 500
 const MAX_SPEED = 120
@@ -7,13 +9,16 @@ const TERMINAL_SPEED = 120
 const FRICTION = 800
 const JUMP_VELOCITY = -200
 
+#controls logs indicating what state the player is in
+const STATE_DEBUG = true
+
 enum {
-	MOVE,
-	ROLL,
-	ATTACK
+	GROUND,
+	JUMP,
+	GRAPPLE
 }
 
-var state = MOVE
+var state = GROUND
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
@@ -22,6 +27,33 @@ onready var animationPlayer = $AnimationPlayer
 	
 	
 func _process(delta):
+	#TODO: make sure jump action can only be performed on ground, or if double jump is enabled
+	if Input.is_action_just_pressed("jump") and state != JUMP:
+		velocity.y = JUMP_VELOCITY
+		state = JUMP
+	
+	match state:
+		GROUND:
+			move_ground(delta)
+			if STATE_DEBUG:
+				print("Ground")
+		JUMP:
+			move_jump(delta)
+			if STATE_DEBUG:
+				print("Jump")
+		GRAPPLE:
+			move_grapple(delta)
+			if STATE_DEBUG:
+				print("Grapple")
+			
+			
+	velocity = move_and_slide(velocity, UP)
+	
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+	#move_and_collide(motion)	
+	
+func move_ground(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector = input_vector.normalized()
@@ -35,26 +67,25 @@ func _process(delta):
 		#animationState.travel("Idle")
 		velocity.x = 0
 		velocity = velocity.move_toward(velocity, FRICTION * delta)
-		
-	#TODO: make sure jump action can only be performed on ground, or if double jump is enabled
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = JUMP_VELOCITY
-	velocity = move_and_slide(velocity)
 	
+func move_jump(delta):
 	
-	"""match state:
-		MOVE:
-			move_state(delta)
-		ROLL:
-			pass
-		ATTACK:
-			attack_state(delta)"""
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector = input_vector.normalized()
+	if input_vector != Vector2.ZERO:
+		#animationTree.set("parameters/Idle/blend_position", input_vector)
+		#animationTree.set("parameters/Run/blend_position", input_vector)
+		#animationTree.set("parameters/Attack/blend_position", input_vector)
+		#animationState.travel("Run")
+		velocity.x = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta).x
 	
-func _physics_process(delta):
-	velocity.y += delta * GRAVITY
-	#move_and_collide(motion)	
-	
-		
+	if is_on_floor() and velocity.y >= 0:
+		state = GROUND
+
+func move_grapple(delta):
+	pass
+			
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -72,13 +103,3 @@ func move_state(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
-	
-	if Input.is_action_just_pressed("attack"):
-		state = ATTACK
-	
-func attack_state(delta):
-	velocity = Vector2.ZERO
-	#animationState.travel("Attack")
-	
-func attack_animation_finished():
-	state = MOVE
