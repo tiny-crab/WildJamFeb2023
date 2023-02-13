a42edd6a566054f315d1e4550d303996d80652f4
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2023-02-12 21:25:39 -0800

    Add physics to grappling hook
    Adds a first pass of physics based movement when grappling. The force is
    determine by the vector pointing from the player to the grapple point.
    Various velocities are altered for better feeling controls.

--- Player.gd
+++ Player.gd
@@ -8,9 +8,10 @@ const MAX_SPEED = 120
 const TERMINAL_SPEED = 120
 const FRICTION = 800
 const JUMP_VELOCITY = -200
+const CHAIN_PULL = 105
 
 #controls logs indicating what state the player is in
-const STATE_DEBUG = true
+const STATE_DEBUG = false
 
 enum {
     GROUND,
@@ -20,17 +21,17 @@ enum {
 
 var state = GROUND
 var velocity = Vector2.ZERO
+var hook_velocity = Vector2.ZERO
 var interactable = null
 
 onready var animationPlayer = $AnimationPlayer
-#onready var animationTree = $AnimationTree
-#onready var animationState = animationTree.get("parameters/playback")
+onready var grapplingHook = $GrappleHook
     
 signal player_can_interact(area)
 signal player_interacted(area)
    
 func _process(delta):
-    #TODO: make sure jump action can only be performed on ground, or if double jump is enabled
+    #MOVEMENT
     match state:
         GROUND:
             move_ground(delta)
@@ -45,42 +46,41 @@ func _process(delta):
             if STATE_DEBUG:
                 print("Grapple")
             
-    if Input.is_action_just_pressed("jump") and state != JUMP:
+    if Input.is_action_just_pressed("jump") and state == GROUND:
         velocity.y = JUMP_VELOCITY
+        state = JUMP
+        
+    if Input.is_action_just_pressed("grapple"):
+        print("grappling")
+        grapplingHook.shoot(get_local_mouse_position())
+        state = GRAPPLE
+        
+    if Input.is_action_just_released("grapple"):
+        grapplingHook.release()
+        state = JUMP    
                 
     velocity = move_and_slide(velocity, UP)
     
+    
+    #INTERACTIONS
     if Input.is_action_just_pressed("interact") and interactable != null:
         print("Interacted with %s" % interactable.name)
         emit_signal("player_interacted", interactable)
         
     
 func _physics_process(delta):
-    velocity.y += delta * GRAVITY
-    #move_and_collide(motion)	
+    if state == JUMP:
+        velocity.y += delta * GRAVITY
     
 func move_ground(delta):
     var input_vector = Vector2.ZERO
     input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
     input_vector = input_vector.normalized()
     if input_vector != Vector2.ZERO:
-        #animationTree.set("parameters/Idle/blend_position", input_vector)
-        #animationTree.set("parameters/Run/blend_position", input_vector)
-        #animationTree.set("parameters/Attack/blend_position", input_vector)
-        #animationState.travel("Run")
         velocity.x = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta).x
     else:
-        #animationState.travel("Idle")
         velocity.x = 0
         velocity = velocity.move_toward(velocity, FRICTION * delta)
-        
-    if Input.is_action_just_pressed("jump") and state != JUMP:
-        velocity.y = JUMP_VELOCITY
-        state = JUMP
-        
-    if Input.is_action_just_pressed("grapple"):
-        print("grappling")
-        state = GRAPPLE		
     
 func move_jump(delta):
     
@@ -88,18 +88,27 @@ func move_jump(delta):
     input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
     input_vector = input_vector.normalized()
     if input_vector != Vector2.ZERO:
-        #animationTree.set("parameters/Idle/blend_position", input_vector)
-        #animationTree.set("parameters/Run/blend_position", input_vector)
-        #animationTree.set("parameters/Attack/blend_position", input_vector)
-        #animationState.travel("Run")
         velocity.x = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta).x
     
     if is_on_floor() and velocity.y >= 0:
         state = GROUND
 
 func move_grapple(delta):
-    var mouse_position = get_local_mouse_position()
-    print(mouse_position.normalized())
+    var input_vector = Vector2.ZERO
+    input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+    input_vector = input_vector.normalized()
+    
+    hook_velocity = to_local(grapplingHook.tip).normalized() * CHAIN_PULL * delta
+    hook_velocity.x *= 1.65
+    velocity += hook_velocity
+    if sign(input_vector.x) != sign(hook_velocity.x):
+        hook_velocity.x *= 0.7
+    
+    
+    
+
+
+# INTERACTIONS
 
 func _on_InteractHitbox_area_entered(area):
     var node = area.get_owner()
--- Player/Grappling Hook/GrappleHook.gd
+++ Player/Grappling Hook/GrappleHook.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const SPEED = 50
+const MAX_LENGTH = 700
 
 onready var chain = $ChainSprite
 
@@ -24,3 +25,20 @@ func _process(delta):
     if not self.visible:
         return
     var tip_loc = to_local(tip)
+    
+    chain.rotation = self.position.angle_to_point(tip_loc) - deg2rad(90)
+    $Hook.rotation = self.position.angle_to_point(tip_loc) - deg2rad(90)
+    chain.position = tip_loc
+    chain.region_rect.size.y = tip_loc.length()
+    
+    if tip_loc.length() >= MAX_LENGTH:
+        flying = false
+    
+func _physics_process(delta):
+    $Hook.global_position = tip
+    if flying:
+        if $Hook.move_and_collide(direction * SPEED):
+            hooked = true
+            flying = false
+    tip = $Hook.global_position
+    
