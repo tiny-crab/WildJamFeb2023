50cb3ded561920a9c3d589a4d6fb042230950257
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2023-02-15 23:00:56 -0800

    Make shrines differentiated and animated

--- Props/Shrine.gd
+++ Props/Shrine.gd
@@ -2,12 +2,20 @@ extends Node2D
 class_name Shrine
 
 export(bool) var isCurseShrine = true
+var animationString = ""
 
 func get_class():
     return "Shrine"
 
+func _ready():
+    animationString = "PlayerNearCurse" if isCurseShrine else "PlayerNearBoon"
+    $AnimatedSprite.play(animationString)
+    $AnimatedSprite.stop()
+
 func _on_InteractHurtbox_area_entered(area):
     $InteractNotification.show()
+    $AnimatedSprite.play(animationString)
 
 func _on_InteractHurtbox_area_exited(area):
     $InteractNotification.hide()
+    $AnimatedSprite.stop()
