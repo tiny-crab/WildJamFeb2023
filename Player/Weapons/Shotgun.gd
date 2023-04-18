980f2bb4319c33d8ec07b2d476484d65a4ad97c6
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2023-04-17 20:03:48 -0700

    Update shotgun to use conical hitbox

--- Player/Weapons/Shotgun.gd
+++ Player/Weapons/Shotgun.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 const BASE_DAMAGE = 20
 const MIN_DAMAGE = 1
+const ZONE_DISTANCES = [50, 75, 110]
+const ZONE_MULTIPLIERS = [1.5, 1, 0.75]
 const DAMAGE_DROPOFF_MODIFIER = 2
 
-onready var raycast = $RayCast2D
 onready var shotgunSprite = $ShotgunSprite
 onready var animationPlayer = $AnimationPlayer
 
@@ -21,10 +22,19 @@ func alter_damage(damage_to_add):
 
 func shoot():
     animationPlayer.play("Fire")
-    if raycast.is_colliding():
-        var hit_object = raycast.get_collider()
-        #TODO make list of hittable scenes and tack that way if we have more than one enemey type
-        if hit_object.get_class() == "Minion" or hit_object.get_class() == "Boss":
-            var knockback_direction = raycast.get_collision_normal()
+
+func _on_Hitbox_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+    var hit_object = body
+    if hit_object.get_class() == "Minion" or hit_object.get_class() == "Boss":
+        var distance = global_position.distance_to(hit_object.global_position)
+        var zone_multiplier = 0
+        for i in range(ZONE_DISTANCES.size()):
+            var zone = ZONE_DISTANCES[i]
+            if distance < zone:
+                zone_multiplier = ZONE_MULTIPLIERS[i]
+                break
+        if zone_multiplier > 0:
+            var knockback_direction = global_position.direction_to(hit_object.global_position)
             knockback_direction.x *= -1
-            hit_object.take_damage(damage, knockback_direction * knockback_force)
+            hit_object.take_damage(damage * zone_multiplier, knockback_direction * knockback_force)
+
